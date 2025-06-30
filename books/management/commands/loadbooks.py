50620dd47ebcdf07ed3d7ac87c8604b977f338be
khomyakov362 <khomyakov362@gmail.com>
2025-06-30 21:14:10 +0500

    loadbooks command now checks if the book is already in the database

--- books/management/commands/loadbooks.py
+++ books/management/commands/loadbooks.py
@@ -21,6 +21,13 @@ class Command(BaseCommand):
 
                 book_dict = make_book_dict(path, settings.REPO_DATA_DIR, xml_str)
 
+                file_name = book_dict['file_name']
+                is_there = Book.objects.filter(file_name=file_name).exists()
+
+                if is_there:
+                    print(f'Book {book_dict["title"]} is already in the database')            
+                    continue
+
                 html = send_to_convert(settings.TEIGARAGE, xml_str)
 
                 if not html:
